86441eb26120e89f4ecc5eba74d0edbc7da074b1
ray <raykrishardi@gmail.com>
2019-04-16 16:05:40 +1000

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Ray Krishardi Layadi on 16/4/19.
+//  Copyright © 2019 Ray Krishardi Layadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
