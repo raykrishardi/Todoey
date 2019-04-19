a11bb87090b1dc2bb0d95ee4ec0a2843e864800f
ray <raykrishardi@gmail.com>
2019-04-19 16:12:23 +1000

    Add category view

---
+++ Todoey/Controller/CategoryTableViewController.swift
@@ -0,0 +1,76 @@
+//
+//  CategoryTableViewController.swift
+//  Todoey
+//
+//  Created by Ray Krishardi Layadi on 19/4/19.
+//  Copyright © 2019 Ray Krishardi Layadi. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryTableViewController: UITableViewController {
+    
+    var categories: [Category] = []
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+
+        loadCategories()
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+
+        cell.textLabel?.text = categories[indexPath.row].name
+
+        return cell
+    }
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Add new category", message: "", preferredStyle: .alert)
+        
+        alert.addTextField { (textField) in
+            textField.placeholder = "Enter new category"
+        }
+        
+        let addAction = UIAlertAction(title: "Add", style: .default) { (alertAction) in
+            let textField = alert.textFields![0] as UITextField
+            
+            self.categories.append(Category(name: textField.text!, context: self.context))
+            self.saveCategories()
+            self.tableView.reloadData()
+        }
+        
+        alert.addAction(addAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func saveCategories() {
+        do {
+            try context.save()
+        } catch {
+            print("Error saving context: \(error)")
+        }
+    }
+    
+    func loadCategories() {
+        let request: NSFetchRequest<Category> = Category.fetchRequest()
+        do {
+            categories = try context.fetch(request)
+        } catch {
+            print("Error fetching from context: \(error)")
+        }
+    }
+    
+}
